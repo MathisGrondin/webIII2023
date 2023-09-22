ad5783131d93161037006a56562bedff63d97b97
TheOtterKaz <mae.grenier.01@edu.cegeptr.qc.ca>
2023-09-22 08:55:24 -0400

    changement index

--- projMiSess/index.php
+++ projMiSess/index.php
@@ -13,15 +13,15 @@
         <div class="row d-flex justify-content-center align-items-center" id="rowCarte">
             <div class="offset col-xl-2 col-m-2"></div>
             <div class="col-xl-8 col- col-m-8 col-12">
-                <div class="card border border-1 border-secondary">
+                <div class="card border-bleuCegep">
                     <div class="card-header bg bg-bleuCegep">
                         <h1 class="fontCegep lilasCegep text-center">Appréciation de l'événement</h1>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body bg bgLilasCegep">
                         <div class="row text-center my-5">
                         <div class="offset col-1"></div>
                             <div class="col-2">
-                                    <a href="add.php?addN=1" class="btn btn-success w-100 border border-1 border-secondary rounded">
+                                    <a href="add.php?addN=1" class="btn btn-success w-100 border border-3 border-light rounded">
                                         <div class="d-flex justify-content-between flex-column w-100 h-100">
                                             <img src="icones/aime.png" alt="aime" class="imgAvis">
                                             <p class="p-0 m-0 fw-bold">Adore</p>
@@ -31,7 +31,7 @@
                         <div class="offset col-1"></div>
                         <div class="offset col-1"></div>
                             <div class="col-2">
-                                <a href="add.php?addN=2" class="btn btn-warning w-100 h-100 border border-1 border-secondary rounded">
+                                <a href="add.php?addN=2" class="btn btn-warning w-100 h-100 border border-3 border-light rounded">
                                     <div class="d-flex justify-content-between flex-column w-100 h-100">
                                         <img src="icones/neutre.png" alt="neutre" class="imgAvis">
                                         <p class="p-0 m-0 fw-bold">Neutre</p>
@@ -41,7 +41,7 @@
                             <div class="offset col-1"></div>
                             <div class="offset col-1"></div>
                             <div class="col-2">
-                                <a href="add.php?addN=3" class="btn btn-danger w-100 h-100 border border-1 border-secondary rounded">
+                                <a href="add.php?addN=3" class="btn btn-danger w-100 h-100 border border-3 border-light rounded">
                                     <div class="d-flex justify-content-between flex-column w-100 h-100">
                                         <img src="icones/deteste.png" alt="deteste" class="imgAvis">
                                         <p class="p-0 m-0 fw-bold">Déteste</p>
