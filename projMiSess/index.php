5759eabd24f4d7903781fff88bdc54e9657d2258
TheOtterKaz <mae.grenier.01@edu.cegeptr.qc.ca>
2023-09-23 15:13:39 -0400

    index changements

--- projMiSess/index.php
+++ projMiSess/index.php
@@ -9,13 +9,14 @@
     <title>Formulaire satisfaction 2023</title>
 </head>
 <body>
+    <!-- Carte étudiant -->
     <div class="container-fluid" id="conteneurCarte">
         <div class="row d-flex justify-content-center align-items-center" id="rowCarte">
             <div class="offset col-xl-2 col-m-2"></div>
             <div class="col-xl-8 col- col-m-8 col-12">
                 <div class="card border-bleuCegep">
                     <div class="card-header bg bg-bleuCegep">
-                        <h1 class="fontCegep lilasCegep text-center">Appréciation de l'événement</h1>
+                        <h1 class="fontCegep lilasCegep text-center fw-bold">Appréciation de l'événement</h1>
                     </div>
                     <div class="card-body bg bgLilasCegep">
                         <div class="row text-center my-5">
@@ -50,6 +51,61 @@
                             </div>
                             <div class="offset col-1"></div>
                         </div>
+                    </div>
+                    <div class="card-footer bg bg-bleuCegep py-4 text-center d-flex justify-content-center align-content-center">
+                        <h4 class="fontCegep lilasCegep p-0 m-0 fs-2">Étudiants</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="offset col-xl-2 col-m-2"></div>
+        </div>
+    </div>
+
+    <!-- Carte Employeur -->
+    <div class="container-fluid" id="contCarteEmp">
+        <div class="row d-flex justify-content-center align-items-center" id="rowCarteEmp">
+            <div class="offset col-xl-2 col-m-2"></div>
+            <div class="col-xl-8 col- col-m-8 col-12">
+                <div class="card border-bleuCegep">
+                    <div class="card-header bg bgLilasCegep">
+                        <h1 class="fontCegep bleuCegep text-center fw-bold">Appréciation de l'événement</h1>
+                    </div>
+                    <div class="card-body bg bg-bleuCegep">
+                        <div class="row text-center my-5">
+                        <div class="offset col-1"></div>
+                            <div class="col-2">
+                                    <a href="add.php?addN=1" class="btn btn-success w-100 border border-3 border-light rounded">
+                                        <div class="d-flex justify-content-between flex-column w-100 h-100">
+                                            <img src="icones/aime.png" alt="aime" class="imgAvis">
+                                            <p class="p-0 m-0 fw-bold">Adore</p>
+                                        </div>
+                                    </a>                                
+                            </div>
+                        <div class="offset col-1"></div>
+                        <div class="offset col-1"></div>
+                            <div class="col-2">
+                                <a href="add.php?addN=2" class="btn btn-warning w-100 h-100 border border-3 border-light rounded">
+                                    <div class="d-flex justify-content-between flex-column w-100 h-100">
+                                        <img src="icones/neutre.png" alt="neutre" class="imgAvis">
+                                        <p class="p-0 m-0 fw-bold">Neutre</p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="offset col-1"></div>
+                            <div class="offset col-1"></div>
+                            <div class="col-2">
+                                <a href="add.php?addN=3" class="btn btn-danger w-100 h-100 border border-3 border-light rounded">
+                                    <div class="d-flex justify-content-between flex-column w-100 h-100">
+                                        <img src="icones/deteste.png" alt="deteste" class="imgAvis">
+                                        <p class="p-0 m-0 fw-bold">Déteste</p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="offset col-1"></div>
+                        </div>
+                    </div>
+                    <div class="card-footer bg bgLilasCegep py-4 text-center d-flex justify-content-center align-content-center">
+                        <h4 class="fontCegep bleuCegep p-0 m-0 fs-2">Employeurs</h4>
                     </div>
                 </div>
             </div>
