e1dd22d6e3f9634f5d7db932acf7c9174e551099
TheOtterKaz <mae.grenier.01@edu.cegeptr.qc.ca>
2023-09-24 16:23:24 -0400

    Users Creation

--- projMiSess/pageUsers.php
+++ projMiSess/pageUsers.php
@@ -10,6 +10,74 @@
 else{
     if($_SESSION["admin"] == false){
         $pageUsers = "block";
+        $listeUsers = "block";
+        $boutonRetourUser = "none";
+        $formUserCr = "none";
+    }
+    else{
+        $pageUsers = "block";
+        $listeUsers = "none";
+        $boutonRetourUser = "block";
+        $formUserCr = "block";
+
+        if(isset($_GET["action"])){
+            $action = $_GET["action"];
+
+            if($action == "Modifier"){
+                $pageUsers = "block";
+                $listeUsers = "block";
+                $formUserCr = "none";
+            }
+        }
+
+        if(!isset($_GET["action"]) && $listeUsers == "block"){
+            $pageUsers = "block";
+            $listeUsers = "none";
+            $formUserCr = "block";
+        }
+
+        if(isset($_GET["state"])){
+            $state = $_GET["state"];
+
+            if($state == 0){
+                $formUserCr = "none";
+                $contextBodyCreaUser = "flex";
+                $messageCreaUser = "Utilisateur créé avec succès";
+                echo '
+                    <script>
+                        setTimeout(function()
+                        {
+                            window.location.href = "admin.php?page=users"
+                        },1000);
+                    </script>';
+            }
+            else if($state == 1){
+                $formUserCr = "none";
+                $contextBodyCreaUser = "flex";
+                $messageCreaUser = "Erreur lors de la création de l'utilisateur";
+
+                echo '
+                    <script>
+                        setTimeout(function()
+                        {
+                            window.location.href = "admin.php?page=users"
+                        },1000);
+                    </script>';
+            }
+            else if($state == 2){
+                $formUserCr = "none";
+                $contextBodyCreaUser = "flex";
+                $messageCreaUser = "Merci de remplir tous les champs";
+
+                echo '
+                    <script>
+                        setTimeout(function()
+                        {
+                            window.location.href = "admin.php?page=users"
+                        },1000);
+                    </script>';
+            }
+        }
     }
 }
 
@@ -18,31 +86,31 @@ else{
 
 
 
-    if($page == "users")
-    {
-        $pageUsers = "block";
-        $barreMenuAdmin = "block";
+    // if($page == "users")
+    // {
+    //     $pageUsers = "block";
+    //     $barreMenuAdmin = "block";
 
-        if($_SESSION["admin"] == false){
-            $listeUsers = "block";
-            $boutonRetourUser = "none";
-            $formUserCr = "none";
-        }
-        else{
-            $listeUsers = "none";
-            $boutonRetourUser = "block";
-            $formUserCr = "block";
-        }
+    //     if($_SESSION["admin"] == false){
+    //         $listeUsers = "block";
+    //         $boutonRetourUser = "none";
+    //         $formUserCr = "none";
+    //     }
+    //     else{
+    //         $listeUsers = "none";
+    //         $boutonRetourUser = "block";
+    //         $formUserCr = "block";
+    //     }
 
-        if(isset($_GET["action"])){
-            $action = $_GET["action"];
+    //     if(isset($_GET["action"])){
+    //         $action = $_GET["action"];
 
-            if($action == "Modifier"){
-                $listeUsers = "block";
-                $formUserCr = "none";
-            }
-        }
+    //         if($action == "Modifier"){
+    //             $listeUsers = "block";
+    //             $formUserCr = "none";
+    //         }
+    //     }
 
-    }
+    // }
 
 ?>
