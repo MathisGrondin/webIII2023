3c9a377f4044e73320da03e168b409095163959b
MathisGrondin <grondin.mathis@hotmail.com>
2023-09-24 13:37:55 -0400

    changements

--- projMiSess/pageEvent.php
+++ projMiSess/pageEvent.php
@@ -1,79 +1,169 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] == "GET" &&  !isset($_SESSION["admin"], $_SESSION["user"]))
-{
-    header("Location: admin.php");
-}
 
-
-// SI DÉSIRE ALLER SUR PAGE Événements
-
-    $pageEvent = "block";
-
-    if(!isset($_GET["action"]) && $afficherliste == "block")
+// Si va sur la page events sans être connecté
+    if($_SERVER["REQUEST_METHOD"] == "GET" &&  !isset($_SESSION["admin"], $_SESSION["user"]))
     {
+        header("Location: admin.php");
+    }
+
+// Sinon, si l'utilisateur est connecté 
+else 
+{
+    if($_SESSION["admin"] == false){
+        $pageEvent = "block";
+        $afficherliste = "block";
+        $formCreation = "none";
+    }
+    else{
         $pageEvent = "block";
         $afficherliste = "none";
         $formCreation = "block";
-    }
 
+        if(isset($_GET["action"]))
+        {
+            $action = $_GET["action"];
+        
+            if($action == "Modifier") 
+            {
+                $pageEvent = "block";
+                $afficherliste = "block";
+                $formCreation = "none";
+            }
+        }
 
-    if(isset($_GET["action"]))
-    {
-        $action = $_GET["action"];
-    
-        if($action == "Modifier") 
+        if(!isset($_GET["action"]) && $afficherliste == "block")
         {
             $pageEvent = "block";
-            $afficherliste = "block";
-            $formCreation = "none";
+            $afficherliste = "none";
+            $formCreation = "block";
         }
-    }  
-    
-    if(isset($_GET["state"])){
-        $state = $_GET["state"];
 
-        if($state == 0){
-            $formCreation = "none";
-            $contextBodyCreaEvent = "flex";
-            $messageContexte = "Événement créé avec succès";
-            echo '
+        if(isset($_GET["state"])){
+            $state = $_GET["state"];
+
+            if($state == 0){
+                $formCreation = "none";
+                $contextBodyCreaEvent = "flex";
+                $messageContexte = "Événement créé avec succès";
+                echo '
+                    <script>
+                        setTimeout(function()
+                        {
+                            window.location.href = "admin.php?page=events"
+                        },1000);
+                    </script>';
+            }
+            else if($state == 1){
+                $formCreation = "none";
+                $contextBodyCreaEvent = "flex";
+                $messageContexte = "Erreur lors de la création de l'événement";
+
+                echo '
                 <script>
                     setTimeout(function()
                     {
                         window.location.href = "admin.php?page=events"
                     },1000);
                 </script>';
-        }
-        else if($state == 1){
-            $formCreation = "none";
-            $contextBodyCreaEvent = "flex";
-            $messageContexte = "Erreur lors de la création de l'événement";
 
-            echo '
-            <script>
-                setTimeout(function()
-                {
-                    window.location.href = "admin.php?page=events"
-                },1000);
-            </script>';
+            }
+            else if($state == 2){
+                $formCreation = "none";
+                $contextBodyCreaEvent = "flex";
+                $messageContexte = "Merci de remplir tous les champs";
 
-        }
-        else if($state == 2){
-            $formCreation = "none";
-            $contextBodyCreaEvent = "flex";
-            $messageContexte = "Merci de remplir tous les champs";
+                echo '
+                <script>
+                    setTimeout(function()
+                    {
+                        window.location.href = "admin.php?page=events"
+                    },1000);
+                </script>';
 
-            echo '
-            <script>
-                setTimeout(function()
-                {
-                    window.location.href = "admin.php?page=events"
-                },1000);
-            </script>';
-
+            }
         }
     }
+}
+
+
+// SI DÉSIRE ALLER SUR PAGE Événements
+
+    // $pageEvent = "block";
+
+    // if($_SESSION["admin"] == false){
+    //     $afficherliste = "block";
+    //     $formCreation = "none";
+    // }
+    // else{
+    //     $afficherliste = "none";
+    //     $formCreation = "block";
+    // }
+
+    // if(!isset($_GET["action"]) && $afficherliste == "block")
+    // {
+    //     $pageEvent = "block";
+    //     $afficherliste = "none";
+    //     $formCreation = "block";
+    // }
+
+
+    // if(isset($_GET["action"]))
+    // {
+    //     $action = $_GET["action"];
+    
+    //     if($action == "Modifier") 
+    //     {
+    //         $pageEvent = "block";
+    //         $afficherliste = "block";
+    //         $formCreation = "none";
+    //     }
+    // }  
+    
+    // if(isset($_GET["state"])){
+    //     $state = $_GET["state"];
+
+    //     if($state == 0){
+    //         $formCreation = "none";
+    //         $contextBodyCreaEvent = "flex";
+    //         $messageContexte = "Événement créé avec succès";
+    //         echo '
+    //             <script>
+    //                 setTimeout(function()
+    //                 {
+    //                     window.location.href = "admin.php?page=events"
+    //                 },1000);
+    //             </script>';
+    //     }
+    //     else if($state == 1){
+    //         $formCreation = "none";
+    //         $contextBodyCreaEvent = "flex";
+    //         $messageContexte = "Erreur lors de la création de l'événement";
+
+    //         echo '
+    //         <script>
+    //             setTimeout(function()
+    //             {
+    //                 window.location.href = "admin.php?page=events"
+    //             },1000);
+    //         </script>';
+
+    //     }
+    //     else if($state == 2){
+    //         $formCreation = "none";
+    //         $contextBodyCreaEvent = "flex";
+    //         $messageContexte = "Merci de remplir tous les champs";
+
+    //         echo '
+    //         <script>
+    //             setTimeout(function()
+    //             {
+    //                 window.location.href = "admin.php?page=events"
+    //             },1000);
+    //         </script>';
+
+    //     }
+    // }
 
 
 
